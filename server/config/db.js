e82c301c0e8d7e13e63a39cf8d2e24c122890421
chenyutong <chenyutong@haixianglicai.com>
2019-04-02 12:25:55 +0800

    fix 数据库配置

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,8 @@
 const Sequelize = require('sequelize'); // 引入sequelize
 
 // 使用url连接的形式进行连接，注意将root: 后面的XXXX改成自己数据库的密码
-// const Todolist = new Sequelize('mysql://root:chenyutong123@39.107.236.111/todolist',{
-// const Todolist = new Sequelize('mysql://root:chenyutong123@localhost/todolist',{
-const blogs = new Sequelize('mysql://root:chenyutong123@localhost/blogs',{
+// const blogs = new Sequelize('mysql://root:chenyutong123@localhost/blogs',{
+const blogs = new Sequelize('mysql://root:chenyutong123@39.107.236.111/blogs',{
   define: {
     timestamps: false // 取消Sequelzie自动给数据表加入时间戳（createdAt以及updatedAt）
   }
