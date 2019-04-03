f0d9a0668b02abc755750557b710def0f6b4031c
chenyutong <chenyutong@haixianglicai.com>
2019-04-03 11:35:48 +0800

    更改sequlize配置

--- server/config/db.js
+++ server/config/db.js
@@ -2,9 +2,10 @@ const Sequelize = require('sequelize'); // 引入sequelize
 
 // 使用url连接的形式进行连接，注意将root: 后面的XXXX改成自己数据库的密码
 // const blogs = new Sequelize('mysql://root:chenyutong123@localhost/blogs',{
-const blogs = new Sequelize('mysql://root:chenyutong123@39.107.236.111/blogs',{
+const blogs = new Sequelize('mysql://chenyutong:chenyutong123@39.107.236.111/blogs',{
   define: {
-    timestamps: false // 取消Sequelzie自动给数据表加入时间戳（createdAt以及updatedAt）
+    timestamps: false, // 取消Sequelzie自动给数据表加入时间戳（createdAt以及updatedAt）
+    dialect: 'sqlite', storage: ':memory:' // 弃用警告
   }
 })
 
